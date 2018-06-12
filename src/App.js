b2de853bd39d26d48d4fdd0c270df44b6299c6a1
pzc <paul.zimmerman.clayton@gmail.com>
2018-06-12 12:49:35 -0500

    working on router, getting error

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import Sidebar from "./sidebar/Sidebar";
 import Home from "./home/Home";
 import Gallery from "./gallery/Gallery";
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="app__container">
         
         {/* Grid CSS Nav */}
@@ -17,11 +19,13 @@ class App extends Component {
 
         {/* GridCSS "Main", will contain either Login, Home, a form, or Gallery */}
         <main>
-          {/* <Home /> */}
-          <Gallery />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/home" component={Home} />
+          
         </main>
 
       </div>
+      </Router>
     );
   }
 }
--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Sidebar.css';
+import { BrowserRouter as Link } from 'react-router-dom'
 
 class Sidebar extends Component {
   constructor(props) {
@@ -9,9 +10,13 @@ class Sidebar extends Component {
   render() { 
     return ( 
       <div className="sidebar__container">
-        <div className="sidebar__button sidebar__home_button">Home</div>
+      
+        <div className="sidebar__button sidebar__home_button"><Link to='/home'>Home</Link></div>
+      
         <div className="sidebar__button sidebar__add_button">Add</div>
+        <Link to='/gallery'>
         <div className="sidebar__button sidebar__viewall_button">View All</div>
+        </Link>
         <div className="sidebar__button sidebar__settings_button">Settings</div>
       </div>
      )

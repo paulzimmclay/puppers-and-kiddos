37a20d3b1423121c8bc5d02a0e67fb4327c208f0
pzc <paul.zimmerman.clayton@gmail.com>
2018-06-20 10:23:51 -0500

    image upload works, but i don't know how and it's ugly

--- src/home/homenav/addnewimagepost/AddNewImagePost.js
+++ src/home/homenav/addnewimagepost/AddNewImagePost.js
@@ -2,14 +2,48 @@
 
 import React, { Component } from "react";
 import apiURL from "../../../DB";
+import Dropzone from "react-dropzone";
+import request from "superagent";
+
+const CLOUDINARY_UPLOAD_PRESET = "tychdmns";
+const CLOUDINARY_UPLOAD_URL =
+  "https://api.cloudinary.com/v1_1/df1kli6hv/upload";
 
 class AddNewImagePost extends Component {
   constructor(props) {
     super(props);
     this.state = {
       caption: "",
-      image: ""
+      image: "",
+      uploadedFileCloudinaryUrl: ""
     };
+  }
+
+  onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+
+  handleImageUpload(file) {
+    let upload = request
+      .post(CLOUDINARY_UPLOAD_URL)
+      .field("upload_preset", CLOUDINARY_UPLOAD_PRESET)
+      .field("file", file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== "") {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
   }
 
   /* "child": 1,
@@ -35,7 +69,7 @@ class AddNewImagePost extends Component {
       body: JSON.stringify({
         family: this.props.currentUserFamilyId,
         caption: this.state.caption,
-        image: this.state.image,
+        image: this.state.uploadedFileCloudinaryUrl,
         family: this.props.currentUserFamilyId,
         category: +this.props.match.params.categoryid,
         date: new Date()
@@ -45,32 +79,41 @@ class AddNewImagePost extends Component {
 
   render() {
     return (
-      <form onSubmit={this.submitImagePostForm}>
-        <label>
-          Image:
-          <input
-            type="text"
-            value={this.state.image}
-            onChange={this.handleImageChange}
-          />
-          <input
-            name="file"
-            type="file"
-            class="file-upload"
-            data-cloudinary-field="image_id"
-            data-form-data="{ 'transformation': {'crop':'limit','tags':'samples','width':3000,'height':2000}}"
-          />
-        </label>
-        <label>
-          Caption:
-          <input
-            type="text"
-            value={this.state.caption}
-            onChange={this.handleCaptionChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="addnewimagepost__container">
+        <form onSubmit={this.submitImagePostForm}>
+          <label>
+            Image:
+            <input
+              type="text"
+              value={this.state.image}
+              onChange={this.handleImageChange}
+            />
+            <Dropzone
+              multiple={false}
+              accept="image/*"
+              onDrop={this.onImageDrop.bind(this)}
+            >
+              <p>Drop an image or click to select a file to upload.</p>
+            </Dropzone>
+          </label>
+          <label>
+            Caption:
+            <input
+              type="text"
+              value={this.state.caption}
+              onChange={this.handleCaptionChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <div>
+        {this.state.uploadedFileCloudinaryUrl === '' ? null :
+        <div>
+          <p>{this.state.uploadedFile.name}</p>
+          <img src={this.state.uploadedFileCloudinaryUrl} />
+        </div>}
+      </div>
+      </div>
     );
   }
 }

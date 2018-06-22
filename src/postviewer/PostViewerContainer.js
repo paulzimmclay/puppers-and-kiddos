aad19418e438e98368a55bb14ff0b0f9211c5f0a
pzc <paul.zimmerman.clayton@gmail.com>
2018-06-22 15:55:14 -0500

    need to get routing for viewer

--- src/postviewer/PostViewerContainer.js
+++ src/postviewer/PostViewerContainer.js
@@ -1,16 +1,60 @@
-// This module will contain an array of posts that will be passed to the PostViewer, plus 
+// Click on a particular gallery post will link to PostViewerContainer, passing that post's category and ID
+// Post cateogry and ID will be used to load from DB into state
+
+// This module will contain an array of posts that will be passed to the PostViewer, plus
 // Eventhandlers that will allow for arrow navigation between posts.
 
-import React, { Component } from 'react';
+// Arrow keys to go forward and back, escape returns to gallery
+
+import React, { Component } from "react";
+import apiURL from '../DB'
 
 class PostViewerContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      posts: [],
+      currentPost: 0
+    };
   }
-  render() { 
-    return (  )
+ 
+  componentDidMount() {
+
+  }
+
+  postFinder = () => {
+    let apiPosts = "";
+    if (
+      this.props.match.params.family === "all" &&
+      this.props.match.params.category === "all"
+    ) {
+      apiPosts = "posts";
+    } else if (this.props.match.params.category === "all") {
+      apiPosts = `posts?family=${this.props.match.params.family}`;
+    } else {
+      apiPosts = `posts?family=${this.props.match.params.family}&category=${
+        this.props.match.params.category
+      }`;
+    }
+    fetch(`${apiURL}/${apiPosts}`)
+      .then(r => r.json())
+      .then(r => {
+        this.setState({
+          posts: r
+        });
+      });
+  };
+
+
+  render() {
+    return (
+      //Grid for Pictures, plus arrows for nav
+      <div className="postviewercontainer__container">
+        {/* <PostViewer /> */}
+        <p className="thing">hi</p>
+      </div>
+    );
   }
 }
- 
+
 export default PostViewerContainer;
